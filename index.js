14c54ac6b233bae09e92377b0d5b739636c78bb2
knfierro <knfierro5@gmail.com>
2025-05-30 09:58:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let playerOne = prompt("What is your name?");
+let playerTwo = prompt("What is your opponent's name?");
+
+document.getElementById("playerOne").textContent = playerOne;
+document.getElementById("playerTwo").textContent = playerTwo;
+
+document.getElementById("rollButton").addEventListener("click", function () {
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  let diceImage1 = "images/dice" + randomNumber1 + ".png";
+  let diceImage2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelector(".img1").setAttribute("src", diceImage1);
+  document.querySelector(".img2").setAttribute("src", diceImage2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.getElementById("rollButton").textContent = `${playerOne} Wins!`;
+  } else if (randomNumber1 < randomNumber2) {
+    document.getElementById("rollButton").textContent = `${playerTwo} Wins!`;
+  } else {
+    document.getElementById("rollButton").textContent = "It's a Draw. Play Again";
+  }
+});
